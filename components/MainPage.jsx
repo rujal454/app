7fd85506956408174ddc3912cb529289fb36df56
rujal <rujalbangdiwala@gmail.com>
2023-08-29 19:43:09 +0530

    commit

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MainPage() {
   return (
-    <div className='text-4xl  bg-gradient-to-r from-indigo-500'>MainPage</div>
+    <div className='text-4xl h-screen w-full flex flex-col bg-gradient-to-r from-indigo-500'>MainPage</div>
   )
 }
 
